5c845b52c963251e0ece3ad64f1aec4bbb90b9d8
SEOKKAMONI <sj01048260279@gmail.com>
2023-01-14 21:01:01 +0900

    ADD :: Search Bar

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,21 @@
 import * as S from "./Main.style.js";
 import Header from "../Header";
+import xSvg from "../../assets/img/x.svg";
+import searchSvg from "../../assets/img/search.svg";
 
 const Main = () => {
   return (
-    <S.Main>
+    <>
       <Header />
-    </S.Main>
+      <S.Main>
+        {/* main */}
+        <S.SearchFrame>
+          <S.SearchBox />
+          <S.Icon src={xSvg} />
+          <S.Icon src={searchSvg} />
+        </S.SearchFrame>
+      </S.Main>
+    </>
   );
 };
 
--- src/components/Main/Main.style.js
+++ src/components/Main/Main.style.js
@@ -1,4 +1,38 @@
 /* your css code */
 import styled from "styled-components";
 
-export const Main = styled.div``;
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+// search
+
+export const SearchFrame = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 1180px;
+  height: 48px;
+  border-radius: 24px;
+  padding-right: 24px;
+
+  border: 1px solid black;
+
+  margin-top: 36px;
+`;
+
+export const SearchBox = styled.input`
+  border-radius: 24px 0px 0px 24px;
+  height: 100%;
+  width: 90%;
+  padding-left: 20px;
+  border: none;
+  outline: none;
+`;
+
+export const Icon = styled.img`
+  cursor: pointer;
+`;
