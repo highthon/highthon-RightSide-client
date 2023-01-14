32dd5b4becfc9ea1e05ec9176f1779fcf61b1309
SEOKKAMONI <sj01048260279@gmail.com>
2023-01-15 01:57:44 +0900

    ADD :: notice write publishing

--- src/components/Notice/NoticeWrite/NoticeWrite.style.js
+++ src/components/Notice/NoticeWrite/NoticeWrite.style.js
@@ -1,10 +1,77 @@
 /* your css code */
 import styled from "styled-components";
+import * as P from "../../../shared/styles/palette.style";
 
-export const NoticeWrite = styled.form``;
-
-export const Input = styled.input`
-  display: block;
+export const NoticeWrite = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
-export const SubmitBtn = styled.button``;
+export const NoticeModal = styled.div`
+  width: 1180px;
+  height: 708px;
+  border-radius: 16px;
+  background-color: #fff;
+  border: 1px solid ${P.palette.hr};
+  border-radius: 16px;
+`;
+
+export const ModalHeader = styled.div`
+  display: flex;
+  align-items: center;
+  height: 80px;
+  border-bottom: 1px solid ${P.palette.hr};
+  border-radius: 16px 16px 0px 0px;
+  padding: 0px 48px;
+`;
+
+export const Title = styled.div`
+  margin-top: 48px;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 28px;
+  color: #000;
+`;
+
+export const ModalContent = styled.form`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const InputFrame = styled.div``;
+
+export const TitleInput = styled.input`
+  margin-top: 16px;
+  padding: 0px 16px;
+  width: 1080px;
+  height: 54px;
+  border: 1px solid ${P.palette.hr};
+  outline: none;
+`;
+
+export const ContentInput = styled.textarea`
+  margin-top: 16px;
+  padding: 16px;
+  min-width: 1080px;
+  min-height: 250px;
+  resize: none;
+  border: 1px solid ${P.palette.hr};
+  outline: none;
+`;
+
+export const SubmitButton = styled.button`
+  width: 100px;
+  height: 44px;
+  border-radius: 40px;
+  background-color: ${P.palette.primaryLight};
+  color: #fff;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 24px;
+  margin: 64px 0px 34px 0px;
+`;
--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Join from "../auth/Join/Join";
 import Login from "../auth/Login/Login";
+import NoticeWrite from "../Notice/NoticeWrite/NoticeWrite";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Join />} />
+        <Route path="/" element={<NoticeWrite />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
